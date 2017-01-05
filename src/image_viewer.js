2e2ed3228417a7847b4164e4eb44af2491c9f056
Hernan Y.Ke <yuriqiao@gmail.com>
2017-01-04 22:24:29 -0500

    lec23 import images to js

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,4 +1,6 @@
-import '../styles/image_viewer.css'
+import '../styles/image_viewer.css';
+import small from '../assets/small.jpg';
+import big from '../assets/big.jpg';
 const image = document.createElement('img');
 image.src = 'http://lorempixel.com/400/400';
 document.body.appendChild(image);
